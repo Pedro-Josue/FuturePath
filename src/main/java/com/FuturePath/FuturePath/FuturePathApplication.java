4a050c0d7a6bb3a4a07b982eb236bea4b31c64d3
Pedro Josué <pedro.josue.ap@outlook.com>
2025-11-10 21:08:16 -0300

    Configuração inicial do projeto

---
+++ src/main/java/com/FuturePath/FuturePath/FuturePathApplication.java
@@ -0,0 +1,13 @@
+package com.FuturePath.FuturePath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuturePathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuturePathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FuturePath/FuturePath/FuturePathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FuturePath.FuturePath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuturePathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
